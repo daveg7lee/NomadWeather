96b8719f7c8e62dfd2f27f686848c8d04b805a56
Dave <daveg7lee@gmail.com>
2021-10-07 20:08:41 +0900

    Location

--- App.js
+++ App.js
@@ -1,37 +1,81 @@
-import React from 'react';
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
 export default function App() {
+  const [city, setCity] = useState('Loading');
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+  };
+
+  const getLocation = async () => {
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 6 });
+    setLocation({ longitude, latitude });
+    getCityName(latitude, longitude);
+  };
+
+  const getCityName = async (latitude, longitude) => {
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+    if (ok) {
+      getLocation();
+    }
+  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>Daegu</Text>
-      </View>
-      <ScrollView
-        contentContainerStyle={styles.weather}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-      >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
+      {ok ? (
+        <>
+          <View style={styles.city}>
+            <Text style={styles.cityName}>{city}</Text>
+          </View>
+          <ScrollView
+            contentContainerStyle={styles.weather}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          >
+            <View style={styles.day}>
+              <Text style={styles.temp}>27</Text>
+              <Text style={styles.description}>Sunny</Text>
+            </View>
+            <View style={styles.day}>
+              <Text style={styles.temp}>27</Text>
+              <Text style={styles.description}>Sunny</Text>
+            </View>
+            <View style={styles.day}>
+              <Text style={styles.temp}>27</Text>
+              <Text style={styles.description}>Sunny</Text>
+            </View>
+            <View style={styles.day}>
+              <Text style={styles.temp}>27</Text>
+              <Text style={styles.description}>Sunny</Text>
+            </View>
+          </ScrollView>
+        </>
+      ) : (
+        <View>
+          <Text>We need permission...</Text>
         </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </ScrollView>
+      )}
     </View>
   );
 }
@@ -47,7 +91,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   cityName: {
-    fontSize: 58,
+    fontSize: 48,
     fontWeight: '500',
   },
   weather: {},
